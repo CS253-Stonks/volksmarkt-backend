7dc3a0357cf4d3a535699ce324dabe217e974c05
Harsh-Murdeshwar <harsharun21@iitk.ac.in>
2023-04-18 00:25:08 +0530

    api to fetch buyer/seller details

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
      path('buyer/register/',views.buyer_register,name='buyer_register'),
      path('seller/login/',views.seller_login,name='seller_login'),
      path('seller/register/',views.seller_register,name='seller_register'),
+     path('buyer/details/<int:pk>/',views.buyer_details.as_view(),name='buyer_details'),
+     path('seller/details/<int:pk>/',views.seller_details.as_view(),name='seller_details'),
      re_path(r'^api/v1/', include('djoser.urls')),
      re_path(r'^api/v1/', include('djoser.urls.authtoken')),
 ]
